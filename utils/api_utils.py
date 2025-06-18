98dc6ea4108d1cc87d981b5ec072b81d3f36b58d
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2025-06-18 18:02:59 -0400

    added a dictionary, a tuple, and a list. A formatted, pretty interval forecast is returned and printed.

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -1,4 +1,5 @@
 from utils.api_key import API_KEY
+from datetime import datetime
 import requests
 
 
@@ -27,14 +28,66 @@ def get_the_weather_data(city):
 
     latitude = lat_lon_data[0]["lat"]
     longitude = lat_lon_data[0]["lon"]
+    geolocation_data = (latitude, longitude)
 
     weather_data_url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=imperial'
     weather_res = requests.get(weather_data_url)
     weather_data_for_city = weather_res.json()
     print(weather_data_for_city)
 
-    print(f"""Current weather in {weather_data_for_city['name']} is:
-          Temperature: {weather_data_for_city['main']['temp']} F
-          Current Conditions: {weather_data_for_city['weather'][0]['description']}
-          Feels like: {weather_data_for_city['main']['feels_like']}
+    weather_info = hold_weather_data(weather_data_for_city)
+    forecast_info = forecast(geolocation_data, weather_info['city'])
+
+    print(f"""Current weather in {weather_info['city']} is:
+          Temperature: {weather_info['temperature']} F
+          Current Conditions: {weather_info['description']}
+          Feels like: {weather_info['feels_like']}
+          Wind speed: {weather_info['wind_speed']} MPH
+          Latitude and Longitude of {weather_info['city']} is {weather_info['coordinates']}
           """)
+
+
+def hold_weather_data(data):
+    return {
+        "city": data.get("name"),
+        "temperature": data["main"].get("temp"),
+        "description": data["weather"][0].get("description"),
+        "feels_like": data["main"].get("feels_like"),
+        "wind_speed": data["wind"].get("speed"),
+        "coordinates": (data["coord"]["lat"], data["coord"]["lon"])
+    }
+
+
+def forecast(my_fancy_tuple, city):
+    # unpack my tuple:
+    lat, lon = my_fancy_tuple
+    forecast_url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}&units=imperial"
+    response = requests.get(forecast_url)
+    if response.status_code != 200:
+        print(
+            f"OpenWeatherAPI request failed with status code {response.status_code}")
+        return []
+    forecast_res = response.json()
+
+    print("Keys in forecast response: ", forecast_res.keys())
+
+    forecast_info = []
+    for day in forecast_res.get("list", [])[:5]:
+        ugly_date = day["dt_txt"]
+        date_to_strip = datetime.strptime(ugly_date, "%Y-%m-%d %H:%M:%S")
+        day_and_time = date_to_strip.strftime("%B %d at %I %p")
+
+        temp = day["main"]["temp"]
+        description = day["weather"][0]["description"]
+
+        forecast_info.append({
+            "dayandtime": day_and_time,
+            "temperature": temp,
+            "description": description
+        })
+
+    print(f"\nOpenWeather 3 Hour Futurecast for {city}: \n")
+    for forecast in forecast_info:
+        print(
+            f"{forecast['dayandtime']}: {forecast['description'].capitalize()} -  {round(forecast['temperature'])}")
+    return forecast_info
