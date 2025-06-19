945536997a0f866538430b395f7fe411e1dab5cc
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2025-06-18 22:45:16 -0400

    There is now a filter feature accepting input and filtering cities from a list of cities that are greater than or equal to that temperature.

---
+++ utils/api_filter.py
@@ -0,0 +1,59 @@
+import requests
+from utils.api_key import API_KEY
+
+# Run this with  python -m utils.api_filter bc it's a module :)
+
+
+def is_it_hot_in_the_city(city):
+    open_weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=imperial"
+    res = requests.get(open_weather_url)
+    if res.status_code != 200:
+        print(
+            f"Open weather request returned an HTTP status code of {res.status_code}")
+        return None
+    data = res.json()
+    # print(f"This is main from the api_filter file {data["main"]}")
+    return data["main"]["temp"]
+
+
+def filter_the_cities(input_temp):
+    list_of_cities = [
+        "Orlando", "Miami", "Raleigh", "Seattle", "Memphis", "Columbus", "Milwaukee", "Albany", "Tulsa", "Jackson", "Cleveland", "Holden Beach", "Lexington", "Minneapolis", "Boise", "Denver", "Joplin", "Amarillo", "Tucson"
+    ]
+
+    print(
+        f"Please wait while I search for cities with a temperature above {input_temp}")
+
+    cities_that_match_input = []
+
+    for city in list_of_cities:
+        current_temperature = is_it_hot_in_the_city(city)
+        if current_temperature is None:
+            continue
+        if current_temperature >= input_temp:
+            cities_that_match_input.append((city, round(current_temperature)))
+
+    return cities_that_match_input
+
+
+def main():
+    try:
+        input_temp = float(input(
+            "Please enter a temperature to check what cities are at or above that value: ")).__round__()
+    except ValueError:
+        print("Geez, try again with a valid number please! ")
+        return
+
+    valid_ciites = filter_the_cities(input_temp)
+
+    if not valid_ciites:
+        print("Did not find any cities with temperatures that high! ")
+        return
+
+    print("Cities that match your vibe: ")
+    for city, current_temperature in valid_ciites:
+        print(f"{city}- {current_temperature}*F")
+
+
+if __name__ == "__main__":
+    main()
